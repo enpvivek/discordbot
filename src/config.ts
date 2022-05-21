3a875314d94d067f1441af80cdae07c31115a16f
Vivek Yadav <enpvivek@gmail.com>
2022-05-21 23:37:07 +0530

    Fixed minor bugs

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const TOKEN = process.env.TOKEN || '';
-export const APPLICAION_ID = process.env.APPLICATION_ID || '';
+export const APPLICATION_ID = process.env.APPLICATION_ID || '';
 export const GUILD_ID = process.env.GUILD_ID || '';
