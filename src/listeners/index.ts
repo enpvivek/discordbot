a478e5bfb7ebbef3c26d12809fc10c97df6461fa
Vivek Yadav <enpvivek@gmail.com>
2022-05-21 22:59:06 +0530

    Added basic commands

---
+++ src/listeners/index.ts
@@ -0,0 +1,3 @@
+export { onReady } from './on-ready';
+export { onMessageCreate } from './on-message-create';
+export { onInteractionCreate } from './on-interaction-create';
---
+++ src/slash-commands/index.ts
@@ -0,0 +1,4 @@
+import { SlashCommand } from '../types';
+import { HelloCommand } from './hello';
+
+export const SlashCommands: SlashCommand[] = [HelloCommand];
